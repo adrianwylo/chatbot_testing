7634e197d1cf9e72897938cdb264ef083a8ade93
blob <adrianwylo1@gmail.com>
2023-08-01 04:11:39 -0500

    correct api

--- bot.py
+++ bot.py
@@ -1,10 +1,13 @@
 import os
 import openai
 
-openai.api_type = "azure"
 openai.api_base = "https://test-ai-insurance.openai.azure.com/"
+openai.api_key = "81b41771820741638eac156eb53bb053"
+openai.api_type = "azure"
 openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv("8a55f4d848e841cba22354e78789ada4")
+
+deployment_name = 'CustomDataBotTest'  # This will correspond to the custom name you chose for your deployment when you deployed a model.
+
 
 class SimpleChatbot:
     def __init__(self):
@@ -13,9 +16,11 @@ class SimpleChatbot:
         ]
 
     def generate_response(self, user_input):
-        self.messages.append({"role": "user", "content": user_input.lower()})
+        self.messages.append({"role": "user", "content": user_input})
+        print("Next response processing")
+
         response = openai.ChatCompletion.create(
-            engine="text-davinci-002",  # Use an appropriate engine here.
+            engine="CustomDataBotTest",
             messages=self.messages,
             temperature=0.7,
             max_tokens=800,
@@ -25,19 +30,26 @@ class SimpleChatbot:
             stop=None
         )
         self.messages.append({"role": "assistant", "content": response["choices"][0]["message"]["content"]})
+        print("Response Tokens:", response["choices"][0]["message"]["content"])  # Print the token count of the response
         return response["choices"][0]["message"]["content"]
+
+    def display_messages(self):
+        print(self.messages)
+
 
 def main():
     chatbot = SimpleChatbot()
     print("Chatbot: Hello! I'm a simple chatbot. How can I assist you?")
     
-    while True:
+    for _ in range(10):  # Change the maximum number of iterations as needed
         user_input = input("You: ")
         if user_input.lower() == 'exit':
             print("Chatbot: Goodbye! Have a great day.")
             break
         response = chatbot.generate_response(user_input)
         print("Chatbot:", response)
+        chatbot.display_messages()
+    print("Chatbot: Goodbye! Have a great day. bit.")
 
 if __name__ == "__main__":
     main()
